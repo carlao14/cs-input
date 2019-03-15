dee5e33a5768204b9d4ba82b55e13e91ff9f500a
carlao14 <34284499+carlao14@users.noreply.github.com>
2019-03-15 17:39:12 -0300

    Create CsInput.vue

---
+++ CsInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-bind:class="defineClasse">
+        <label for=""> {{label}} </label>
+        <input class="form-control" type="text" v-on:input="$emit('input', $event.target.value)">
+    </div>
+
+</template>
+
+<script>
+  export default {
+    props: ['label','tamanho'],
+    data() {
+        return {
+            
+        }
+    },
+     mounted() {
+            console.log('Component mounted.')
+        },
+    computed: {
+        defineClasse: function(){
+            return "form-group "+ (this.tamanho || "form-group col-md-6 col-lg-6");
+          },
+    },
+    methods: {
+        
+    }
+    }
+</script>
